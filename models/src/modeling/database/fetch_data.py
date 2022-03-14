d28a8e57de3dd1767e4636c7d269f2d68b57e457
Vaibhav Gupta <vvg5125@psu.edu>
2022-03-14 02:59:49 -0400

    Training Models

--- models/src/modeling/database/fetch_data.py
+++ models/src/modeling/database/fetch_data.py
@@ -1,30 +1,49 @@
 import pandas as pd
 import pyodbc
-import keys
-from keys import username, password, server, driver, database
 import os
 
+server = 'sustprediction.database.windows.net'
+database = 'sustainability'
+username = 'capstone'
+password = '{ds440@grp6}'   
+driver= '{ODBC Driver 17 for SQL Server}'
 
-conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+def chicago():
+    conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
-# AQI Chicago
-rows1 = conn.cursor().execute("SELECT County, AQI_Weekly_Measurement, AQI_week, AQI_month, AQI_year, AQI_lat, AQI_long, City FROM dbo.Chicago_AQI").fetchall() 
+    # AQI Chicago
+    rows1 = conn.cursor().execute("SELECT County, AQI_Weekly_Measurement, AQI_week, AQI_month, AQI_year, AQI_lat, AQI_long, City FROM dbo.Chicago_AQI").fetchall() 
 
-# Features Chicago
-rows2 = conn.cursor().execute("SELECT feature_week, feature_month, feature_year, feature_lat, feature_long, feature_name, feature_val FROM dbo.ChicagoFeatures").fetchall() 
+    # Features Chicago
+    rows2 = conn.cursor().execute("SELECT feature_week, feature_month, feature_year, feature_lat, feature_long, feature_name, feature_val FROM dbo.ChicagoFeatures").fetchall() 
 
-#Convert to df AQI
-dfAQI = pd.DataFrame([tuple(t) for t in rows1], columns=['County', 'AQI_Weekly_Measurement', 'AQI_week', 'AQI_month', 'AQI_year', 'AQI_lat', 'AQI_long', 'City']) 
+    #Convert to df AQI
+    dfAQI = pd.DataFrame([tuple(t) for t in rows1], columns=['County', 'AQI_Weekly_Measurement', 'AQI_week', 'AQI_month', 'AQI_year', 'AQI_lat', 'AQI_long', 'City']) 
 
-#Convert to df features
-dfFeatures = pd.DataFrame([tuple(t) for t in rows2], columns=['feature_week', 'feature_month', 'feature_year', 'feature_lat', 'feature_long', 'feature_name', 'feature_val'])
-dfFeatures['AQI_week'] = dfFeatures['feature_week']
-dfFeatures['AQI_month'] = dfFeatures['feature_month']
-dfFeatures['AQI_year'] = dfFeatures['feature_year']
+    #Convert to df features
+    dfFeatures = pd.DataFrame([tuple(t) for t in rows2], columns=['feature_week', 'feature_month', 'feature_year', 'feature_lat', 'feature_long', 'feature_name', 'feature_val'])
+    dfFeatures['AQI_week'] = dfFeatures['feature_week']
+    dfFeatures['AQI_month'] = dfFeatures['feature_month']
+    dfFeatures['AQI_year'] = dfFeatures['feature_year']
 
-data_dir = './data/'
-dfAQI.to_csv(data_dir + 'ChicagoAQI.csv', index = False)
-dfFeatures.to_csv(data_dir + 'ChicagoFeatures.csv', index = False)
+    data_dir = './data/'
+    dfAQI.to_csv(data_dir + 'ChicagoAQI.csv', index = False)
+    dfFeatures.to_csv(data_dir + 'ChicagoFeatures.csv', index = False)
+
+    return dfAQI, dfFeatures
+
+
+def nyc_v2():
+
+    conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+
+    # NYC V2 Data
+    nyc = conn.cursor().execute("SELECT * FROM dbo.newyork_v2").fetchall() 
+    columns = conn.cursor().execute("SELECT * FROM sys.columns WHERE object_id = OBJECT_ID('dbo.newyork_v2')").fetchall()
+    columns_df = pd.DataFrame([tuple(t) for t in columns])
+    list_names = list(columns_df[1])
+    nyc_df = pd.DataFrame([tuple(t) for t in nyc], columns = list_names)
+    return nyc_df
 
 
 
--- models/src/modeling/main.py
+++ models/src/modeling/main.py
@@ -1,1 +1,87 @@
 #-----Main Model File----#
+class Model:
+
+    def __init__(self, data):
+
+        self.data = data
+
+    def preprocess(self):
+
+        self.data['License_Class'] = self.data['License_Class'].astype('category').cat.codes
+        train = self.data[self.data['year'] != 2021]
+        test = self.data[self.data['year'] == 2021]
+        print(f"Train Data Years: {train.year.unique()}")
+        print(f"Test Data Years: {test.year.unique()}")
+
+        X_train = train.drop(['pm25','aqi'], axis = 1)
+        y_train = train[['aqi']]
+        X_test = test.drop(['pm25','aqi'], axis = 1)
+        y_test = test[['aqi']]
+
+        print(f"X_train Shape: {X_train.shape}")
+        print(f"X_test Shape: {X_test.shape}")
+        return X_train, y_train, y_test, X_test, train
+
+    def pca(self, train):
+
+        pca = PCA(n_components=5)
+        pca.fit(train)
+        print(f"explained_variance_ratio_: {pca.explained_variance_ratio_}")
+        print(f"singular_values_:{pca.singular_values_}")
+        print(f"Components: {pca.components_}")
+
+        return pca
+
+    def build_rfr(self, X_train, y_train, y_test, X_test):
+
+        rfr = RandomForestRegressor(n_estimators=200, criterion="mse", 
+                                    min_samples_leaf=3, min_samples_split=3, 
+                                    max_depth=10).fit(X_train, y_train)
+
+        print(f"Random Forest Regressor Score: {rfr.score(X_test, y_test) * 100}")
+
+        return rfr
+
+    def build_NN(self, X_train, y_train, X_test, y_test):
+
+        tf.random.set_seed(42)  #first we set random seed
+        model = tf.keras.Sequential([
+                             tf.keras.layers.Dense(32,activation="relu",input_shape=(32,)),
+                             tf.keras.layers.Dense(1)])
+        model.compile( loss = tf.keras.losses.mae, #mae stands for mean absolute error
+              optimizer = tf.keras.optimizers.SGD(), #stochastic GD
+              metrics = ['mae'])
+        model.fit(X_train.values.reshape(32, 936), y_train.values.reshape(-1 , 936), epochs = 10)
+        predictons = model.evaluate(X_test.values.reshape(32, 318), y_test.values.reshape(-1 ,318))
+
+        return model, predictions
+
+    def plot_preds(self, X_train, y_train, X_test, y_test, model, predictions):
+        
+        plt.figure(figsize=(12,6))
+        plt.scatter(X_train, y_train, c="b", label="Training data")
+        plt.scatter(X_test, y_test, c="g", label="Testing data")
+        plt.scatter(X_test, predictions, c="r", label="Predictions")
+        plt.legend()
+
+        return _
+
+if __name__ == "__main__":
+
+    from database.fetch_data import nyc_v2
+    from sklearn.ensemble import RandomForestRegressor
+    import pandas as pd
+    import warnings
+    from sklearn.decomposition import PCA
+    import tensorflow as tf
+
+    warnings.filterwarnings("ignore")       
+    data = nyc_v2()
+    print(data.head())
+
+    model = Model(data)
+    X_train, y_train, y_test, X_test, train = model.preprocess()
+    pca_analysis  = model.pca(train)
+    model_RFR = model.build_rfr( X_train, y_train, y_test, X_test)
+    model_NN, predictions = model.build_NN( X_train, y_train, y_test, X_test)
+    #plots = model.plot_preds(X_train, y_train, y_test, X_test, model_NN, predictions)
