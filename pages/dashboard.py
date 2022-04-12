a96fdf096964becbf49846035a16be627ef76a16
allyracho <allyr06aqua@yahoo.com>
2022-04-12 09:33:08 -0400

    average monthly AQI added

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,68 +1,78 @@
 # Import Libraries
+import statistics
+
 import pandas as pd
 import streamlit as st
 import tensorflow as tf
 
 
 # PREDICTION FUNCTION
-def predict_AQI(city, week, year):
-
+def predict_AQI(city, week, year, multi_week, month):
     if city == 'Chicago':
-        '''
-        if week == week_dict:
+        data = pd.read_csv("./pages/data/chi_actual_pred.csv")
+        if multi_week:
             result = []
             actual = []
-            for i in week_dict.values():
-                result_val = model.predict(data)
-                result_val = result.loc[(data['week'] == i) & data['year'] == year]
-                actual_val = data.loc[(data['week'] == i) & data['year'] == year]
+            for i in week.values():
+                result_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                result_val = result_val.iloc[:, 1].values
+                actual_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                actual_val = actual_val.iloc[:, 6].values
                 result.append(result_val)
                 actual.append(actual_val)
             return result, actual
-        '''
-        data = pd.read_csv("./pages/data/chi_actual_pred.csv")
-        result = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
-        result = result.iloc[:,1].values
-        actual = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
-        actual = actual.iloc[:,6].values
-        return result, actual
+
+        elif month != '0':
+            result = pd.DataFrame(data[(data["month"] == int(month)) & (data["year"] == int(year))])
+            result = statistics.mean(result.iloc[:, 1].values)
+            actual = pd.DataFrame(data[(data["month"] == int(month)) & (data["year"] == int(year))])
+            actual = statistics.mean(actual.iloc[:, 6].values)
+            return result, actual
+
+        else:
+            result = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
+            result = result.iloc[:, 1].values
+            actual = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
+            actual = actual.iloc[:, 6].values
+            return result, actual
 
     if city == 'Philadelphia':
-        '''
-        if week == week_dict:
+        data = pd.read_csv("./pages/data/phl_actual_pred.csv")
+        if multi_week:
             result = []
             actual = []
-            for i in week_dict.values():
-                result_val = model.predict(data)
-                result_val = result.loc[(data['week'] == i) & data['year'] == year]
-                actual_val = data.loc[(data['week'] == i) & data['year'] == year]
+            for i in week.values():
+                result_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                result_val = result_val.iloc[:, 1].values
+                actual_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                actual_val = actual_val.iloc[:, 7].values
                 result.append(result_val)
                 actual.append(actual_val)
             return result, actual
-        '''
-        data = pd.read_csv("./pages/data/phl_actual_pred.csv")
-        result = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
-        result = result.iloc[:, 1].values
-        actual = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
-        actual = actual.iloc[:, 7].values
-        return result, actual
-    '''
-    if city == 'Hazleton':
-     
-            result = model_haz.predictions4
-            result = result.loc[(scaled_haz_y_test['week'] == week) & scaled_haz_y_test['year'] == year]
-            actual = scaled_haz_y_test.loc[(scaled_haz_y_test['week'] == week) & scaled_haz_y_test['year'] == year]
-            return [result, actual]
-    '''
+
+        elif month != '0':
+            result = pd.DataFrame(data[(data["month"] == int(month)) & (data["year"] == int(year))])
+            result = statistics.mean(result.iloc[:, 1].values)
+            actual = pd.DataFrame(data[(data["month"] == int(month)) & (data["year"] == int(year))])
+            actual = statistics.mean(actual.iloc[:, 7].values)
+            return result, actual
+
+        else:
+            result = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
+            result = result.iloc[:, 1].values
+            actual = pd.DataFrame(data[(data["week"] == int(week)) & (data["year"] == int(year))])
+            actual = actual.iloc[:, 7].values
+            return result, actual
+
 
 
 # APPLICATION FUNCTION
 def app():
     # description
     st.write("This application has been designed using transfer learning"
-             " to predict the AQI values for specific cities. Initially, New York City was "
+             " to predict the AQI values for specific cities. New York City is "
              "the source city of the model and the weights of this city were transferred to "
-             "predict on similar cities of Chicago, IL, Philadelphia, PA, and Hazleton, PA. "
+             "similar cities of Chicago, IL and Philadelphia, PA to predict the AQI. "
              "Below the user will get the chance to chose a city, week, and year and find out the "
              "AQI for the inputs. ")
 
@@ -71,50 +81,71 @@ def app():
 
     # user input variables
     city = st.selectbox("Please enter the city you would like to predict:",
-                        ('Chicago', 'Philadelphia', 'Hazleton'))
+                        ('Chicago', 'Philadelphia'))
     week = st.number_input("Please enter the week of the year you would like to predict:",
                            min_value=1, max_value=53)
     year = st.selectbox("Please enter the year you would like to predict:",
                         ('2018', '2019', '2020', '2021'))
 
-    multi_week = st.checkbox("If you would like multiple weeks of data for a year, please check here")
-    if multi_week:
+    funct = st.selectbox("If you would rather choose multiple weeks of information or a monthly "
+                     "average for a given year, please select one of the following, else keep blank",
+                     ('-', 'Multiple weeks for given year', 'Average AQI for given month and year'))
+
+    if funct == 'Multiple weeks for given year':
         week_dict = {}
         num_weeks = st.number_input("Please enter how many weeks you would like:", min_value=1, max_value=53)
-        for i in range(1, num_weeks):
-            week_dict[i] = st.number_input("Please enter the week of the year you would like to predict:",
-                                           min_value=1, max_value=53)
+        entered_weeks = st.button('Enter')
+        if entered_weeks:
+            count = 1
+            for i in range(0, int(num_weeks)):
+                week_dict[i] = st.number_input("Please enter the week of the year you would like to predict:",
+                                               min_value=1, max_value=53, key=count)
+                count += 1
+    if funct == 'Average AQI for given month and year':
+        month = st.selectbox("Please enter the month you would like to predict:", ('1', '2', '3', '4', '5', '6',
+                                                                                                            '7', '8',
+                                                                                   '9', '10', '11', '12'))
 
     if st.button('Predict'):
         if city == 'Chicago':
-            '''
-            if multi_week:
-                result = predict_AQI(city, week_dict, year, 
-                                     tf.keras.models.load_model('./models/src/modeling/build/Models/Chicago_Model_V1'))
-            '''
-            result = predict_AQI(city, week, year)
-            d = {'lat': [41.8781], 'lon': [-87.6298]}
-            df = pd.DataFrame(d)
-            st.map(df)
-        if city == 'Hazleton':
-            '''
-            if multi_week:
-                result = predict_AQI(city, week_dict, year,tf.keras.models.load_model('./models/src/modeling/build/Models/Hazleton_Model_V1'))
-            '''
-            result = predict_AQI(city, week, year,
-                                 tf.keras.models.load_model('./models/src/modeling/build/Models/Hazleton_Model_V1'))
-            d = {'lat': [40.9584], 'lon': [-75.9746]}
-            df = pd.DataFrame(d)
-            st.map(df)
+            if funct == 'Multiple weeks for given year':
+                result = predict_AQI(city, week_dict, year, True, '0')
+                d = {'lat': [41.965193], 'lon': [-87.876265]}
+                df = pd.DataFrame(d)
+                st.write("Below is the coordinate the AQI was measured at")
+                st.map(df)
+            elif funct == 'Average AQI for given month and year':
+                result = predict_AQI(city, week, year, False, month)
+                d = {'lat': [41.965193], 'lon': [-87.876265]}
+                df = pd.DataFrame(d)
+                st.write("Below is the coordinates the AQI was measured at")
+                st.map(df)
+            else:
+                result = predict_AQI(city, week, year, False, '0')
+                d = {'lat': [41.965193], 'lon': [-87.876265]}
+                df = pd.DataFrame(d)
+                st.write("Below is the coordinate the AQI was measured at")
+                st.map(df)
+
         if city == 'Philadelphia':
-            '''
-            if multi_week:
-                result = predict_AQI(city, week_dict, year, tf.keras.models.load_model('./models/src/modeling/build/Models/Philly_Model_V1'))
-            '''
-            result = predict_AQI(city, week, year)
-            d = {'lat': [39.9526], 'lon': [-75.1652]}
-            df = pd.DataFrame(d)
-            st.map(df)
+            if funct == 'Multiple weeks for given year':
+                result = predict_AQI(city, week_dict, year, True, '0')
+                d = {'lat': [39.988842], 'lon': [-75.207205]}
+                df = pd.DataFrame(d)
+                st.write("Below is the coordinate the AQI was measured at")
+                st.map(df)
+            elif funct == 'Average AQI for given month and year':
+                result = predict_AQI(city, week, year, False, month)
+                d = {'lat': [39.988842], 'lon': [-75.207205]}
+                df = pd.DataFrame(d)
+                st.write("Below is the coordinate the AQI was measured at")
+                st.map(df)
+            else:
+                result = predict_AQI(city, week, year, False, '0')
+                d = {'lat': [39.988842], 'lon': [-75.207205]}
+                df = pd.DataFrame(d)
+                st.write("Below is the coordinate the AQI was measured at")
+                st.map(df)
 
         # predicted and actual aqi
         st.success('The predicted AQI: {}'.format(result[0]))  # predicted AQI for inputted row_num week and year
@@ -129,13 +160,10 @@ def app():
             chi_data = pd.read_csv('./pages/data/Chicago-Sample.csv')
             chi_data = chi_data.iloc[:, 5:]
             st.dataframe(chi_data)
-        elif city == 'Philadelphia':
+        else:
             phl_data = pd.read_csv('./pages/data/Philly-Sample.csv')
             phl_data = phl_data.iloc[:, 5:]
             st.dataframe(phl_data)
-        else:
-            data = pd.read_csv('./pages/data/Haz-Sample.csv')
-            st.dataframe(data.iloc[:,5:])
 
         # input visual graph created by model and RMSE
         st.header('Plot of True Values to Predicted Values')
@@ -143,10 +171,6 @@ def app():
             st.image('pages/images/chi_plot.png')
             st.metric('Normalized RMSE', 0.24682464)
 
-        elif city == 'Philadelphia':
+        else:
             st.image('pages/images/philly_plot.png')
             st.metric('Normalized RMSE', 0.20027714)
-
-        else:
-            st.image('pages/images/haz_plot.png')
-            st.metric('Normalized RMSE', 0.4365)
