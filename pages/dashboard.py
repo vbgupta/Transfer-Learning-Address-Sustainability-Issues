8c80ae838fef30293215755830494f17969a3749
Ally Racho <55199345+allyracho@users.noreply.github.com>
2022-04-14 10:02:18 -0400

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,22 +1,20 @@
 # Import Libraries
 import statistics
-
 import pandas as pd
 import streamlit as st
-import tensorflow as tf
 
 
 # PREDICTION FUNCTION
 def predict_AQI(city, week, year, multi_week, month):
     if city == 'Chicago':
-        data = pd.read_csv("./pages/data/chi_actual_pred.csv")
+        data = pd.read_csv("pages/data/chi_actual_pred.csv")
         if multi_week:
             result = []
             actual = []
             for i in week.values():
-                result_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                result_val = pd.DataFrame(data[(data["week"] == (i)) & (data["year"] == int(year))])
                 result_val = result_val.iloc[:, 1].values
-                actual_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                actual_val = pd.DataFrame(data[(data["week"] == (i)) & (data["year"] == int(year))])
                 actual_val = actual_val.iloc[:, 6].values
                 result.append(result_val)
                 actual.append(actual_val)
@@ -37,14 +35,14 @@ def predict_AQI(city, week, year, multi_week, month):
             return result, actual
 
     if city == 'Philadelphia':
-        data = pd.read_csv("./pages/data/phl_actual_pred.csv")
+        data = pd.read_csv("pages/data/phl_actual_pred.csv")
         if multi_week:
             result = []
             actual = []
             for i in week.values():
-                result_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                result_val = pd.DataFrame(data[(data["week"] == (i)) & (data["year"] == int(year))])
                 result_val = result_val.iloc[:, 1].values
-                actual_val = pd.DataFrame(data[(data["week"] == int([i])) & (data["year"] == int(year))])
+                actual_val = pd.DataFrame(data[(data["week"] == (i)) & (data["year"] == int(year))])
                 actual_val = actual_val.iloc[:, 7].values
                 result.append(result_val)
                 actual.append(actual_val)
@@ -94,13 +92,14 @@ def app():
     if funct == 'Multiple weeks for given year':
         week_dict = {}
         num_weeks = st.number_input("Please enter how many weeks you would like:", min_value=1, max_value=53)
-        entered_weeks = st.button('Enter')
-        if entered_weeks:
-            count = 1
-            for i in range(0, int(num_weeks)):
-                week_dict[i] = st.number_input("Please enter the week of the year you would like to predict:",
-                                               min_value=1, max_value=53, key=count)
-                count += 1
+        count = int(num_weeks)
+        while count != 0:
+            week_dict[count] = ""
+            count -= 1
+        for count,value in week_dict.items():
+            week_dict[count] = int(st.number_input("Please enter the week of the year you"
+                                                " would like to predict:",min_value=1, max_value=53, key=count))
+
     if funct == 'Average AQI for given month and year':
         month = st.selectbox("Please enter the month you would like to predict:", ('1', '2', '3', '4', '5', '6',
                                                                                                             '7', '8',
@@ -148,29 +147,33 @@ def app():
                 st.map(df)
 
         # predicted and actual aqi
-        st.success('The predicted AQI: {}'.format(result[0]))  # predicted AQI for inputted row_num week and year
-        st.warning('The actual AQI: {}'.format(result[1]))  # actual AQI for inputted row_num week and year
+        st.success('**The predicted AQI: {}**'.format(result[0]))  # predicted AQI for inputted row_num week and year
+        st.success('**The actual AQI: {}**'.format(result[1]))  # actual AQI for inputted row_num week and year
 
         # figure of AQI measurements / description
         st.image("./pages/images/PM2017.png")
-
-        # feature row used in modeling
-        st.header("Features used in predicting the AQI value")
-        if city == 'Chicago':
-            chi_data = pd.read_csv('./pages/data/Chicago-Sample.csv')
-            chi_data = chi_data.iloc[:, 5:]
-            st.dataframe(chi_data)
-        else:
-            phl_data = pd.read_csv('./pages/data/Philly-Sample.csv')
-            phl_data = phl_data.iloc[:, 5:]
-            st.dataframe(phl_data)
 
         # input visual graph created by model and RMSE
         st.header('Plot of True Values to Predicted Values')
         if city == 'Chicago':
             st.image('pages/images/chi_plot.png')
             st.metric('Normalized RMSE', 0.24682464)
+            st.write("Normalized RMSE is measuring the standard deviation of the residuals (prediction errors)."
+                     " In general, a RMSE between 0.2 - 0.5 indicates the model relatively predicts accurately.")
 
         else:
             st.image('pages/images/philly_plot.png')
             st.metric('Normalized RMSE', 0.20027714)
+            st.write("Normalized RMSE is measuring the standard deviation of the residuals (prediction errors)."
+                     " In general, a RMSE between 0.2 - 0.5 indicates the model relatively predicts accurately.")
+
+        # feature row used in modeling
+        st.header("Features used in predicting the AQI value")
+        if city == 'Chicago':
+            chi_data = pd.read_csv('pages/data/Chicago-Sample.csv')
+            chi_data = chi_data.iloc[:, 5:]
+            st.dataframe(chi_data)
+        else:
+            phl_data = pd.read_csv('pages/data/Philly-Sample.csv')
+            phl_data = phl_data.iloc[:, 5:]
+            st.dataframe(phl_data)
