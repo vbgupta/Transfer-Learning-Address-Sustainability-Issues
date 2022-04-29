0a1802b5ad500abfdc1fec1d508a99d793be42f3
Ally Racho <55199345+allyracho@users.noreply.github.com>
2022-04-29 10:53:34 -0400

    combining multiple years AQI into one for CHI/PHL

---
+++ prep/api/AQIdata.R
@@ -0,0 +1,51 @@
+#load libraries
+library("jsonlite")
+
+#########################
+# Philadelphia AQI
+##########################
+
+#philly 2018 aqi 
+result <- fromJSON("./Philly/Data/PhillyAQI2018.json")
+phillyAQI2018 <- as.data.frame(result)
+
+#philly 2019 aqi 
+result <- fromJSON("./Philly/Data/PhillyAQI2019.json")
+phillyAQI2019 <- as.data.frame(result)
+
+#philly 2021 aqi
+result <- fromJSON("./Philly/Data/PhillyAQI2020.json")
+phillyAQI2020 <- as.data.frame(result)
+
+#philly 2020 aqi
+result <- fromJSON("./Philly/Data/PhilllyAQI2021.json")
+phillyAQI2021 <- as.data.frame(result)
+
+#combine all years to one dataset
+df_list <- list(phillyAQI2018, phillyAQI2019, phillyAQI2020, phillyAQI2021)
+PhillyAQI <- Reduce(function(x,y)merge(x,y,all = TRUE), df_list, accumulate = FALSE)
+
+
+
+#########################
+# Chicago AQI
+##########################
+#chicago 2018 aqi
+result <- fromJSON("./Chicago/Data/ChicagoAQI2018.json")
+chicagoAQI2018 <- as.data.frame(result)
+
+#chicago 2019 aqi
+result <- fromJSON("./Chicago/Data/ChicagoAQI2019.json")
+chicagoAQI2019 <- as.data.frame(result)
+
+#chicago 2020 aqi
+result <- fromJSON("./Chicago/Data/ChicagoAQI2020.json")
+chicagoAQI2020 <- as.data.frame(result)
+
+#chicago 2021 aqi
+result <- fromJSON("./Chicago/Data/ChicagoAQI2021.json")
+chicagoAQI2021 <- as.data.frame(result)
+
+#combine all years to one dataset
+df_list <- list(chicagoAQI2018, chicagoAQI2019, chicagoAQI2020, chicagoAQI2021)
+ChicagoAQI <- Reduce(function(x,y)merge(x,y,all = TRUE), df_list, accumulate = FALSE)
